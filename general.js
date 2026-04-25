7fd05a2059446a5a1d0d37c6a9d1893ae6dba62e
Khushi Purwar <khushi.purwar23@vit.edu>
2026-04-25 12:50:52 +0530

    improved general.js

--- general.js
+++ general.js
@@ -1,46 +1,41 @@
 const axios = require('axios');
 
-// Get all books
 async function getAllBooks() {
     try {
-        const response = await axios.get('http://localhost:5000/books');
-        console.log(response.data);
-    } catch (error) {
-        console.log(error.message);
+        const res = await axios.get('http://localhost:5000/books');
+        console.log(res.data);
+    } catch (err) {
+        console.error("Error fetching books:", err.message);
     }
 }
 
-// Get book by ISBN
 async function getBookByISBN(isbn) {
     try {
-        const response = await axios.get(`http://localhost:5000/books/isbn/${isbn}`);
-        console.log(response.data);
-    } catch (error) {
-        console.log(error.message);
+        const res = await axios.get(`http://localhost:5000/books/isbn/${isbn}`);
+        console.log(res.data);
+    } catch (err) {
+        console.error("Error:", err.message);
     }
 }
 
-// Get books by Author
 async function getBooksByAuthor(author) {
     try {
-        const response = await axios.get(`http://localhost:5000/books/author/${author}`);
-        console.log(response.data);
-    } catch (error) {
-        console.log(error.message);
+        const res = await axios.get(`http://localhost:5000/books/author/${author}`);
+        console.log(res.data);
+    } catch (err) {
+        console.error("Error:", err.message);
     }
 }
 
-// Get books by Title
 async function getBooksByTitle(title) {
     try {
-        const response = await axios.get(`http://localhost:5000/books/title/${title}`);
-        console.log(response.data);
-    } catch (error) {
-        console.log(error.message);
+        const res = await axios.get(`http://localhost:5000/books/title/${title}`);
+        console.log(res.data);
+    } catch (err) {
+        console.error("Error:", err.message);
     }
 }
 
-// Call functions
 getAllBooks();
 getBookByISBN(1);
 getBooksByAuthor("Author A");
